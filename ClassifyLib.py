fe2d72a6076dcebd279b089ff8e18a3af5bfc59c
LewisEvansMyDinopc <lewis@lewisevans.com>
2017-03-30 08:18:15 +0100

    added util needed

---
+++ ClassifyLib.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import random
+
+############################################################################################################################################################
+
+# value: scalar-int
+def CalcErrorCount (predClasses, trueClasses):
+	assert predClasses.shape == trueClasses.shape
+	errorCount = (trueClasses != predClasses).sum()
+	return errorCount
+
+# value: scalar-float	
+def CalcErrorRate (predClasses, trueClasses):
+	assert predClasses.shape == trueClasses.shape
+	errorRate = CalcErrorCount(predClasses, trueClasses) / (predClasses.shape[0])
+	return errorRate
+		
+############################################################################################################################################################
+
